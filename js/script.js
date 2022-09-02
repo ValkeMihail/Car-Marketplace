2f0286ad86f95b346df0be2110ace309e60c3874
Mihail Nicoară <mihail.ncr@gmail.com>
2022-09-02 15:48:37 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function openLoginForm(){
+  
+  document.body.classList.add("showLoginForm");
+  menu.classList.toggle('menu-open');
+
+}
+function closeLoginForm(){
+  document.body.classList.remove("showLoginForm");  
+}
+function openSellForm(){
+  document.body.classList.add("showSellForm");
+  menu.classList.toggle('menu-open');
+
+}
+function closeSellForm(){
+  document.body.classList.remove("showSellForm");
+
+}
+
+
