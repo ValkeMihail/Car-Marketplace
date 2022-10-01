e8eb2d8248d1413a464e752a317f0e80409d7abc
Mihail Nicoară <mihail.ncr@gmail.com>
2022-10-01 15:42:15 +0300

    commit sell and login pages

---
+++ arrow.js
@@ -0,0 +1,7 @@
+function activate() {
+    var secondSection = document.getElementById("second-section");
+    var firstSection = document.getElementById("first-section");
+    secondSection.classList.remove("inactive-section");
+    // firstSection.style.borderRight = "1px solid lightgreen";
+    // secondSection.style.borderLeft = "1px solid red";
+  }
